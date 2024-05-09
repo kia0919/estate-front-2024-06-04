d4e40f8af65ad1bc327abc41a291ec4e865c7971
kia0919 <john0256@naver.com>
2024-05-09 11:55:30 +0900

    2024.05.09 11:50

--- src/views/service/Local/index.tsx
+++ src/views/service/Local/index.tsx
@@ -1,43 +1,46 @@
 import React from 'react';
-import './style.css';
+import './style.css'
+import SelectBox from 'src/components/Selectbox';
 
-
-//                    component                   //
+//                    component                    //
 export default function Local() {
 
-  //                    render                   //
-  return (
-    <div id='local-wrapper'>
-      <div className='local-top'>
-        <div className='local-search-box'>
-          <div className='select-box'>
-            <div className='select-selected'></div>
-            <div className='select-open-button'></div>
-          </div>
-          <div className='primary-button'>검색</div>
+    //                    render                    //
+    return (
+        <div id='local-wrapper'>
+            <div className='local-top'>
+                <div className='local-search-box'>
+                    <SelectBox />
+                    <div className='primary-button'>검색</div>
+                </div>
+                <div className='local-origin-text'>데이터 출처: KOSIS</div>
+            </div>
+            <div className='local-card'>
+                <div className='local-card-title-box'>
+                    <div className='local-card-title'>매매 평균</div>
+                    <div className='local-card-unit'>(단위: 백만원)</div>
+                </div>
+                <div className='local-card-chart-box'>
+                    
+                </div>
+            </div>
+            <div className='local-card'>
+                <div className='local-card-title-box'>
+                    <div className='local-card-title'>전세 평균</div>
+                    <div className='local-card-unit'>(단위: 백만원)</div>
+                </div>
+                <div className='local-card-chart-box'>
+                    
+                </div>
+            </div><div className='local-card'>
+                <div className='local-card-title-box'>
+                    <div className='local-card-title'>월세 평균</div>
+                    <div className='local-card-unit'>(단위: 백만원)</div>
+                </div>
+                <div className='local-card-chart-box'>
+                    
+                </div>
+            </div>
         </div>
-        <div className='local-origin-text'>데이터 출저: KOSIS</div>
-      </div>
-      <div className='local-card'>
-        <div className='local-card-title-box'>
-          <div className='local-card-title'>매매 평균</div>
-          <div className='local-card-unit'>(단위: 백만원)</div>
-        </div>
-        <div className='local-card-chart-box'>
-          
-        </div>
-      </div>
-      <div className='local-card'>
-        <div className='local-card-title-box'>
-          <div className='local-card-title'>월세 평균</div>
-          <div className='local-card-unit'>(단위: 백만원)</div>
-        </div>
-        <div className='local-card-chart-box'>
-          
-        </div>
-      </div>
-      <div className='local-card'></div>
-      <div className='local-card'></div>
-    </div>
-  )
-};
+    );
+}
